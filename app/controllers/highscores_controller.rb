cae72da1941094ac6b8050847164721cbf15911a
Kira Combs <kicombs@mines.edu>
2012-05-09 16:14:32 -0600

    eliminated unneccesary items in the controller

--- app/controllers/highscores_controller.rb
+++ app/controllers/highscores_controller.rb
@@ -20,23 +20,7 @@ class HighscoresController < ApplicationController
       format.json { render json: @highscore }
     end
   end
-
-  # GET /highscores/new
-  # GET /highscores/new.json
-  def new
-    @highscore = Highscore.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @highscore }
-    end
-  end
-
-  # GET /highscores/1/edit
-  def edit
-    @highscore = Highscore.find(params[:id])
-  end
-
+  
   # POST /highscores
   # POST /highscores.json
   def create
@@ -55,29 +39,4 @@ class HighscoresController < ApplicationController
 
   # PUT /highscores/1
   # PUT /highscores/1.json
-  def update
-    @highscore = Highscore.find(params[:id])
-
-    respond_to do |format|
-      if @highscore.update_attributes(params[:highscore])
-        format.html { redirect_to @highscore, notice: 'Highscore was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @highscore.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /highscores/1
-  # DELETE /highscores/1.json
-  def destroy
-    @highscore = Highscore.find(params[:id])
-    @highscore.destroy
-
-    respond_to do |format|
-      format.html { redirect_to highscores_url }
-      format.json { head :no_content }
-    end
-  end
 end
