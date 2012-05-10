5c3f381aebee3aa897e559bb304beeb9978bd15f
Kira Combs <kicombs@mines.edu>
2012-05-09 21:44:10 -0600

    more fun with the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 Highscore.delete_all
-Highscore.create(name: "Harry Potter", score: 9); 
-Highscore.create(name: "Cthulu", score: 3);
-Highscore.create(name: "Nearly Died", score: 2);
+Highscore.create(name: "Harry Potter", score: 9) 
+Highscore.create(name: "Cthulu", score: 3)
+Highscore.create(name: "Nearly Died", score: 2)
